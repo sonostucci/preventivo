0278c787f1fd50737a6f35014c1a676c999ce90e
sonostucci <scuccaton@gmail.com>
2021-05-11 17:59:48 +0000

    modify

--- app/models/fatture.rb
+++ app/models/fatture.rb
@@ -1,4 +1,14 @@
 class Fatture < ApplicationRecord
-    validates :iva, presence: { message: ": Non puoi lasciare vuoto questo campo" }
+    validates :iva, presence: { message: ": Non puoi lasciare vuoto questo campo"}
+
+    validates :nomelavoro, presence: { message: ": Non puoi lasciare vuoto questo campo" } 
+
+    validates :spettabile, presence: { message: ": Non puoi lasciare vuoto questo campo" } 
+
+    validates :descrizione, presence: { message: ": Non puoi lasciare vuoto questo campo" } 
+
+    validates :quantita, presence: { message: ": Non puoi lasciare vuoto questo campo" } 
+
+    validates :prezzo, presence: { message: ": Non puoi lasciare vuoto questo campo" } 
 
 end
